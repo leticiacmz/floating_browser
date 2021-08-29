68c179eed736a0eaa1054ad931ead23965eb083b
leticiacmz <leticmz@gmail.com>
2021-08-29 18:25:17 -0300

    update: add openDevtools

---
+++ main.js
@@ -0,0 +1,54 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, globalShortcut } = require("electron");
+const path = require("path");
+const config = require("./config");
+
+let win;
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    titleBarStyle: "hidden",
+    alwaysOnTop: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    }
+  });
+  // and load the index.html of the app.
+  win.loadURL("config.url");
+}
+function toggleDevTools() {
+  win.webContents.toggleDevTools();
+}
+
+function createShortcuts() {
+  globalShortcut.register('Ctrl+J', toggleDevTools);
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Algumas APIs podem ser usadas somente depois que este evento ocorre.
+app.whenReady().then(() => {
+  createWindow();
+  createShortcuts();
+
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. Você também pode colocar eles em arquivos separados e requeridos-as aqui.
